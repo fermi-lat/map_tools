bcfc8ce328771c34acb89708cc7b52382175418b
jchiang <>
2004-03-02 01:06:45 +0000

    omit explicit use of FloatImg typedef

--- map_tools/ExposureHyperCube.h
+++ map_tools/ExposureHyperCube.h
@@ -2,14 +2,14 @@
     @brief declare class ExposureHyperCube 
 
     @author Toby Burnett
-    $Header: /nfs/slac/g/glast/ground/cvs/map_tools/map_tools/ExposureHyperCube.h,v 1.3 2004/02/28 14:20:22 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/map_tools/map_tools/ExposureHyperCube.h,v 1.4 2004/02/28 21:04:54 burnett Exp $
 
 */
 
 #ifndef TOOLS_EXPOSUREHYPERCUBE_H
 #define TOOLS_EXPOSUREHYPERCUBE_H
 
-#include "image/Image.h"
+//#include "image/Image.h"
 #include "Exposure.h"
 
 /** @class ExposureHyperCube 
@@ -20,69 +20,22 @@
     */
 class ExposureHyperCube  {
 public:
-    ExposureHyperCube( const Exposure& exp, std::string outfile) : m_image(0)
-    {
+    ExposureHyperCube( const Exposure& exp, std::string outfile);
+   
+   //! @brief add a string or douuble key to the image 
+   void setKey(std::string name, double value, std::string unit="", 
+               std::string comment="");
 
-        std::vector<long> naxes(3);
-        naxes[0]= Exposure::Index::ra_factor;
-        naxes[1]= Exposure::Index::dec_factor;
-        naxes[2] =Exposure::Index::cosfactor;
+   void setKey(std::string name, std::string value,
+               std::string unit="", std::string comment="");
 
-        m_image = new FloatImg("hypercube", outfile, naxes);
-        m_image->data() = exp.data(); 
+   void save(std::string outfile="");
 
-        setKey("TELESCOP", "GLAST");
-        setKey("INSTRUME", "SIMULATION");
- 
-        setKey("DATE-OBS", "");
-        setKey("DATE-END", "");
-        setKey("EQUINOX", 2000.0);
-        
-        setKey("CTYPE1", "RA--CAR");
-        setKey("CRPIX1",  0.5*(1+naxes[0]));
-        setKey("CRVAL1",  0);
-        setKey("CDELT1",  -1.);
-        setKey("CUNIT1",  "deg");
-
-        setKey("CTYPE2",  "DEC--CAR");
-        setKey("CRPIX2",  0.5*(1+naxes[1]));
-        setKey("CRVAL2",  0.);
-        setKey("CDELT2",  1.0);
-        setKey("CUNIT2",  "deg");
-
-        setKey("CTYPE3",  "SQRT(1-COSTHETA)");
-        setKey("CRPIX3",   0.5*(1+naxes[2]) );
-        setKey("CRVAL3",  0.);
-        setKey("CDELT3",  1./naxes[2] );
-        setKey("CUNIT3",  "");
-
-        setKey("BUNIT", "s");
-
-        setKey("CROTA2",  0);
-        // record total exposure as a key
-        setKey("TOTAL", exp.total());
-
-    }
-    //! @brief add a string or douuble key to the image 
-    void setKey(std::string name, double value, std::string unit="", std::string comment="")
-    {
-        m_image->addAttribute(DoubleAttr(name, value, unit, comment)); 
-    }
-    void setKey(std::string name, std::string value,std::string unit="", std::string comment="")
-    {
-        m_image->addAttribute(StringAttr(name, value,unit,comment)); 
-    }
-    void save(std::string outfile=""){
-        m_image->saveElement(outfile);
-        delete m_image;
-        m_image=0;
-    }
-    ~ExposureHyperCube(){
-        if(m_image!=0) save();
-    }
+   ~ExposureHyperCube();
 
 private:
-    FloatImg* m_image;
+//    FloatImg* m_image;
+   void * m_image;
 
 };
 #endif //TOOLS_EXPOSUREHYPERCUBE_H
