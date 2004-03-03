88887c00d74b9f5bc823f12e1338f4dc55368625
jchiang <>
2004-03-03 00:12:14 +0000

    satisfy all Header class unit tests

--- src/image/Header.cxx
+++ src/image/Header.cxx
@@ -4,7 +4,7 @@
  * @authors T. Burnett, J. Chiang
  * Original code from Riener Rohlfs
  *
- * $Header$
+ * $Header: /nfs/slac/g/glast/ground/cvs/map_tools/src/image/Header.cxx,v 1.1 2004/03/02 23:24:57 jchiang Exp $
  */
 
 #include "Header.h"
@@ -18,12 +18,19 @@ Header::~Header() {
 
 void Header::addAttribute(const BaseAttr& attribute, bool replace) {
    std::string name = attribute.name();
-   if (~count(name) || replace) {
+   BaseAttrMap::iterator it = find(name);
+   if (it == end()) {
       insert(BaseAttrMap::value_type(name, attribute.clone()));
-   } else {
       return;
+   } 
+   if (replace) {
+      delete it->second;
+      it->second = attribute.clone();
+      return;
+   } else {
+      throw std::runtime_error("Header::addAttribute: attribute "
+                               + name + " already exists.");
    }
-   return;
 }
 
 const BaseAttr * Header::operator[](const std::string & name) const {
