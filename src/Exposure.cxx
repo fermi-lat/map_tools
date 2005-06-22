317ff1cfc7b9b41afb77c9331cf382b04904bdbe
burnett <>
2005-06-22 17:58:45 +0000

    make sure get last entry when making map

--- src/Exposure.cxx
+++ src/Exposure.cxx
@@ -1,12 +1,14 @@
 /** @file Exposure.cxx
     @brief Implementation of class Exposure
 
-   $Header: /nfs/slac/g/glast/ground/cvs/map_tools/src/Exposure.cxx,v 1.21 2005/03/05 22:51:53 burnett Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/map_tools/src/Exposure.cxx,v 1.22 2005/03/06 20:51:40 burnett Exp $
 */
 #include "map_tools/Exposure.h"
 #include "map_tools/HealpixArrayIO.h"
 #include "tip/Table.h"
 #include "astro/EarthCoordinate.h"
+
+#include <memory>
 
 using namespace map_tools;
 
@@ -107,7 +109,7 @@ bool Exposure::processEntry(const tip::ConstTableRecord & row, const GTIvector& 
 
             if( start < first ) {
                 if( stop < first) continue; // history interval before gti
-                if( stop < second){
+                if( stop <= second){
                     fraction = (stop-first)/(stop-start); // overlap start of gti
                     break;
                 }
@@ -115,7 +117,7 @@ bool Exposure::processEntry(const tip::ConstTableRecord & row, const GTIvector& 
                 break;
             }else {
                 if( start > second) continue; // interval after gti 
-                if( stop < second ) {
+                if( stop <= second ) {
                     fraction = 1.0; break;  // fully contained
                 }
                 fraction = (second-start)/(stop-start);  // overlap end of gti
