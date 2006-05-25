18f41ecce12a5af0d2154c0ded4f0883b60b06ad
burnett <>
2006-05-25 01:24:43 +0000

    new functionality for selection, estimating direction of possible source

--- src/PhotonMap.cxx
+++ src/PhotonMap.cxx
@@ -1,13 +1,14 @@
 /** @file PhotonMap.cxx
 @brief implementation of PhotonMap
 
-$Header: /nfs/slac/g/glast/ground/cvs/map_tools/src/PhotonMap.cxx,v 1.8 2006/05/08 21:30:22 burnett Exp $
+$Header: /nfs/slac/g/glast/ground/cvs/map_tools/src/PhotonMap.cxx,v 1.9 2006/05/15 15:49:41 burnett Exp $
 */
 
 #include "map_tools/PhotonMap.h"
 
 #include <cmath>
 #include <utility>
+#include "CLHEP/Vector/ThreeVector.h"
 
 using namespace map_tools;
 using astro::HealPixel;
@@ -106,6 +107,44 @@ int PhotonMap::extract(const astro::SkyDir& dir, double radius,
     return total;
 }
 
+int PhotonMap::extract(const astro::SkyDir& dir, double radius,
+                       std::vector<std::pair<astro::HealPixel, int> >& vec,
+                       int select_level, bool include_all) const
+{
+    bool allsky(radius>=180); // maybe use to simplify below, but seems fast
+    radius *= (M_PI / 180); // convert to radians
+    if (select_level == -1)
+        select_level = m_minlevel; // default level to select
+    int nside( 1<< select_level);
+    int total(0);
+    vec.clear();
+
+    // Get pixels in select level that are within radius
+    std::vector<int> v;
+    astro::Healpix hpx(nside, astro::Healpix::NESTED, HealPixel::s_coordsys);
+    hpx.query_disc(dir, radius, v);  
+
+    // Add select level pixels to return vector
+    for (std::vector<int>::const_iterator it = v.begin(); it != v.end(); ++it)
+    {
+
+        astro::HealPixel hp(*it, select_level);
+        const_iterator it2 = find(hp);
+		if (it2 == end()) // Not in PhotonMap
+		{
+			if (include_all) // Add anyway
+				vec.push_back(std::make_pair(hp, 0));
+		}
+		else // In PhotonMap
+        {
+            int count = it2->second;
+            vec.push_back(std::make_pair(it2->first, count));
+            total += count;
+        }
+    }
+
+    return total;
+}
 //! Count the photons, perhaps weighted, within a given pixel.
 double PhotonMap::photonCount(const astro::HealPixel & px, bool includeChildren,
                               bool weighted) const
@@ -134,5 +173,28 @@ double PhotonMap::photonCount(const astro::HealPixel & px, bool includeChildren,
         }
         return count;
     }
+}
+
+//! Count the photons within a given pixel, weighted with children.  Also return weighted direction.
+double PhotonMap::photonCount(const astro::HealPixel & px, astro::SkyDir & NewDir) const
+{
+        
+    
+    double count = 0;
+    int maxLevel = m_minlevel + m_levels - 1;
+    astro::HealPixel boundary(px.lastChildIndex(maxLevel), maxLevel);
+	CLHEP::Hep3Vector v(0,0,0);
+
+    for (PhotonMap::const_iterator it = lower_bound(px);
+        it != end() && it->first <= boundary; ++it)
+    {
+        double weight = 1 << 2*(it->first.level() - m_minlevel);
+
+        count += it->second * weight; 
+		v +=  weight * it->second * (it->first)().dir();
+    }
+
+	NewDir = astro::SkyDir(v);
+	return count;
 }
 
